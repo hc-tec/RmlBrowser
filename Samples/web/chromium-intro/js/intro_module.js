6133ce9a93365abbe592562cafc8b87ae0969924
hc-tec <2173324540@qq.com>
2023-01-04 22:10:46 +0800

    improve example: chromium-project

---
+++ Samples/web/chromium-intro/js/intro_module.js
@@ -0,0 +1,44 @@
+
+
+class IntroModule {
+
+    constructor(parent_ele) {
+        this.parent_ele = parent_ele
+        this.modules = []
+    }
+
+    Append(bg_img, title, description) {
+        this.modules.push({
+            bg_img,
+            title,
+            description
+        })
+        return this
+    }
+
+    BuildElement(module) {
+        const intro_module = document.createElement(document, 'div')
+        intro_module.setClassNames('intro-module')
+        intro_module.setAttribute('style', `decorator: image(${module.bg_img} cover);`)
+        const intro_module_container = document.createElement(document, 'div')
+        intro_module_container.setClassNames('intro-module-container')
+        const intro_title = document.createElement(document, 'p')
+        intro_title.innerRML = module.title
+        const intro_description = document.createElement(document, 'p')
+        intro_description.innerRML = module.description
+        intro_module_container.appendChild(intro_module_container, intro_title)
+        intro_module_container.appendChild(intro_module_container, intro_description)
+        intro_module.appendChild(intro_module, intro_module_container)
+        this.parent_ele.appendChild(this.parent_ele, intro_module)
+    }
+
+    Build() {
+        this.modules.forEach(module => {
+            this.BuildElement(module)
+        })
+    }
+
+}
+
+
+
--- Samples/web/chromium-intro/js/main.js
+++ Samples/web/chromium-intro/js/main.js
@@ -1,12 +1,26 @@
 
+// import './intro_module.js'
+
+// press F5, fresh
 document.addEventListener(document, 'keydown', (e) => {
     const params = e.getParameters()
     const key_identifier = params['key_identifier']
-    // press F5, fresh
+    // F5 = 111
     if (key_identifier === 111) {
         reload()
     }
 })
+
+const intro_module_wrapper = document.getElementsByClassName('intro-module-wrapper')[0]
+const intro_module = new IntroModule(intro_module_wrapper)
+intro_module.Append('./images/rI6G204.png', 'Thread', 'The network stack is a mostly single-threaded cross-platform library primarily for resource fetching.')
+    .Append('./images/ss3c252_ANI-4.png', 'Thread', 'The network stack is a mostly single-threaded cross-platform library primarily for resource fetching.')
+    .Append('./images/WAN2698_ANI-1-2.png', 'Thread', 'The network stack is a mostly single-threaded cross-platform library primarily for resource fetching.')
+    .Append('./images/Snipaste_2023-01-04_20-48-41_prev_ui.png', 'Thread', 'The network stack is a mostly single-threaded cross-platform library primarily for resource fetching.')
+    .Append('./images/ndRm753_1212.png', 'Thread', 'The network stack is a mostly single-threaded cross-platform library primarily for resource fetching.')
+    .Build()
+
+
 
 
 
