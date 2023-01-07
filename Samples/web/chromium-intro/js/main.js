35bdb5433d18c47b757eadf276bf716dd3f2c749
hc-tec <2173324540@qq.com>
2023-01-07 15:23:21 +0800

    import coost lib

--- Samples/web/chromium-intro/js/main.js
+++ Samples/web/chromium-intro/js/main.js
@@ -11,14 +11,7 @@ document.addEventListener(document, 'keydown', (e) => {
     }
 })
 
-const intro_module_wrapper = document.getElementsByClassName('intro-module-wrapper')[0]
-const intro_module = new IntroModule(intro_module_wrapper)
-intro_module.Append('./images/rI6G204.png', 'Thread', 'The network stack is a mostly single-threaded cross-platform library primarily for resource fetching.')
-    .Append('./images/ss3c252_ANI-4.png', 'Thread', 'The network stack is a mostly single-threaded cross-platform library primarily for resource fetching.')
-    .Append('./images/WAN2698_ANI-1-2.png', 'Thread', 'The network stack is a mostly single-threaded cross-platform library primarily for resource fetching.')
-    .Append('./images/Snipaste_2023-01-04_20-48-41_prev_ui.png', 'Thread', 'The network stack is a mostly single-threaded cross-platform library primarily for resource fetching.')
-    .Append('./images/ndRm753_1212.png', 'Thread', 'The network stack is a mostly single-threaded cross-platform library primarily for resource fetching.')
-    .Build()
+
 
 
 
