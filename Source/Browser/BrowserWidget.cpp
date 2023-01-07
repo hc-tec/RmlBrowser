78bef12b258334fccc4ee0a889d84d0cbd6a7d85
hc-tec <2173324540@qq.com>
2023-01-07 23:17:08 +0800

    init browser basic widgets

---
+++ Source/Browser/BrowserWidget.cpp
@@ -0,0 +1,5 @@
+//
+// Created by titto on 2023/1/7.
+//
+
+#include "BrowserWidget.h"
---
+++ Source/Browser/BrowserWidget.h
@@ -0,0 +1,15 @@
+//
+// Created by titto on 2023/1/7.
+//
+
+#ifndef RMLUI_BROWSERWIDGET_H
+#define RMLUI_BROWSERWIDGET_H
+
+class BrowserWidget {
+public:
+
+private:
+
+};
+
+#endif // RMLUI_BROWSERWIDGET_H
